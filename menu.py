6db40fe86d640a24b550455dbecbb52201c6a1cb
jiemingg <sbluesea@qq.com>
2017-10-05 20:06:22 +0800

    improve menu.py

--- menu.py
+++ menu.py
@@ -6,31 +6,31 @@ from basic import Token
 class Menu(object):
     def __init__(self):
         pass
+
     def create(self, postData, accessToken):
         postUrl = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % accessToken
         if isinstance(postData, unicode):
             postData = postData.encode('utf-8')
         urlResp = urllib.urlopen(url=postUrl, data=postData)
-        print urlResp.read()
+        return urlResp.read()
 
     def query(self, accessToken):
         postUrl = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=%s" % accessToken
         urlResp = urllib.urlopen(url=postUrl)
-        print urlResp.read()
+        return urlResp.read()
 
     def delete(self, accessToken):
         postUrl = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=%s" % accessToken
         urlResp = urllib.urlopen(url=postUrl)
-        print urlResp.read()
+        return urlResp.read()
         
     #获取自定义菜单配置接口
     def get_current_selfmenu_info(self, accessToken):
         postUrl = "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=%s" % accessToken
         urlResp = urllib.urlopen(url=postUrl)
-        print urlResp.read()
+        return urlResp.read()
 
 if __name__ == '__main__':
-    myMenu = Menu()
     postJson = """
     {
         "button":
@@ -105,6 +105,6 @@ if __name__ == '__main__':
           ]
     }
     """
+    myMenu = Menu()
     accessToken = Token().get_access_token()
-    #myMenu.delete(accessToken)
-    myMenu.create(postJson, accessToken)
+    print myMenu.create(postJson, accessToken)
