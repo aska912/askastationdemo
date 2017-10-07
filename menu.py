f786ad3bc997a428acb39e6346f7839d9e7b53e1
jiemingg <sbluesea@qq.com>
2017-10-07 23:36:21 +0800

    add 'parse xml' for menu

--- menu.py
+++ menu.py
@@ -1,11 +1,66 @@
 ﻿# -*- coding: utf-8 -*-
 # filename: menu.py
-import urllib
+import urllib, json
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
 from basic import Token
 
 class Menu(object):
     def __init__(self):
-        pass
+        self.xmlfile="menu.xml"
+
+    def readConfig(self):
+        cfgXmlData = self.parseXml()
+        if len(cfgXmlData) == 0:
+            return None
+        else:
+            return json.dumps(cfgXmlData, ensure_ascii=False, encoding="utf-8")
+
+    def parseXml(self):
+        menuDict = {}
+        menuTree = ET.parse(self.xmlfile)
+        menuRoot = menuTree.getroot()
+        menuTag = menuRoot.tag
+        if menuTag == "menu": 
+            menuTag="button"
+            menuDict = {menuTag: []}
+        else:
+            return menuDict
+        for button in menuRoot:
+            btnName = button.attrib['name'].encode('utf-8')
+            if isinstance(button.find('subbutton'), Element):
+                btnDict = {'name': btnName, "sub_button": []}
+                for subutton in button:
+                    subtnName = subutton.attrib['name'].encode('utf-8')
+                    subtnType = subutton.find('type').text
+                    subtnArgName = self.keyOrUrl(subtnType)
+                    subtnArg = subutton.find(subtnArgName).text
+                    subtnDict = {'name': subtnName, \
+                                 'type': subtnType, \
+                                 subtnArgName: subtnArg}
+                    btnDict["sub_button"].append(subtnDict)
+            else:
+                btnType = button.find('type').text
+                btnArgName = self.keyOrUrl(btnType)
+                btnArg = button.find(btnArgName).text
+                btnDict = {'name': btnName, \
+                           'type': btnType, \
+                           btnArgName: btnArg}
+            menuDict[menuTag].append(btnDict)
+        return menuDict
+            
+
+    def keyOrUrl(self, btnType):
+        if btnType == "view":
+            return "url"
+        elif btnType == "click"             or \
+             btnType == "scancode_waitmsg"  or \
+             btnType == "scancode_push"     or \
+             btnType == "location_select"   or \
+             btnType == "pic_weixin"        or \
+             btnType == "pic_sysphoto"      or \
+             btnType == "pic_photo_or_album":
+            return "key"
 
     def create(self, postData, accessToken):
         postUrl = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % accessToken
@@ -107,4 +162,4 @@ if __name__ == '__main__':
     """
     myMenu = Menu()
     accessToken = Token().get_access_token()
-    print myMenu.create(postJson, accessToken)
+    print myMenu.create(myMenu.readConfig(), accessToken)
